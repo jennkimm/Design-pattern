9d9180675e38660750596f473d21f2ee90b0d8c2
smartshk <smartshk98@gmail.com>
2019-11-26 10:27:46 +0900

    add file ch17 - Observer pattern

---
+++ ch17/Sample/Observer.java
@@ -0,0 +1,10 @@
+package ch17.Sample;
+
+// 관찰자를 표현하는 인터페이스
+public interface Observer {
+	// NumberGenerator가 “나의 내용이 갱신되었습니다. 
+	// 표시도 갱신해주세요” 라고 관찰자에게 알려줄 때 호출하는 메소드
+	public abstract void update(NumberGenerator generator); //관찰대상 자체를 받겠다.
+	//public abstract void update(int num); //생성된 숫자만 받겠다
+	
+}
